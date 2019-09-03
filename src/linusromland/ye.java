8af174e015d3eec3038d27fb24fa54333c3f1eae
linusromland <linus.romland@gmail.com>
2019-09-03 11:58:38 +0200

    added class and some code
    very nice

---
+++ src/linusromland/ye.java
@@ -0,0 +1,17 @@
+package linusromland;
+
+import java.util.Scanner;
+
+public class ye {
+
+	System.out.println("Vill du sova?");
+	Scanner scanner = new Scanner(System.in);
+	String input = scanner.nextLine();
+	
+	if (input == "ja"){
+		System.out.println("Jag med");
+	}
+
+	
+}
+}
