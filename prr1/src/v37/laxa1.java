15d2d0f2e73e9642d01f372448d69ad9f5bb50f0
Linus Romland <romlandlinus@gmail.com>
2019-09-10 12:01:53 +0200

    Scanner added

--- prr1/src/v37/laxa1.java
+++ prr1/src/v37/laxa1.java
@@ -1,10 +1,13 @@
 package v37;
 
+import java.util.Scanner;
+
 public class laxa1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
 	}
 
 }
