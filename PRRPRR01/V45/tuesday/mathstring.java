309641098c67d0d0a82fb7fcaa35acef93732602
linusromland <linus.romland@vastkustit.se>
2019-11-05 11:30:59 +0100

    massa grejer

--- PRRPRR01/V45/tuesday/mathstring.java
+++ PRRPRR01/V45/tuesday/mathstring.java
@@ -1,6 +1,23 @@
 package tuesday;
 
+import java.util.Scanner;
+
 public class mathstring {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double a = 3;
+        double b = 2;
+
+        System.out.println("test");
+
+        double svar = Math.sqrt((Math.pow(a, b)));
+
+    String mening = "cool";
+        String mening2 = input.nextLine();
+
+        if (mening.equals(mening)){
+            System.out.println("samma");
+        }
     }
 }
